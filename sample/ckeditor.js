4eaf533ef1f5f120ff991a2b6fdc892db5022fa9
Kota Nakamoto <cinemaster.125@gmail.com>
2024-03-23 21:42:15 +0900

    update sample html

--- sample/ckeditor.js
+++ sample/ckeditor.js
@@ -2,20 +2,11 @@ import CKEditorInspector from '@ckeditor/ckeditor5-inspector';
 
 import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
 
-import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
-import { Bold, Code, Italic } from '@ckeditor/ckeditor5-basic-styles';
-import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
-import { CodeBlock } from '@ckeditor/ckeditor5-code-block';
+import { Bold } from '@ckeditor/ckeditor5-basic-styles';
 import { Essentials } from '@ckeditor/ckeditor5-essentials';
 import { Heading } from '@ckeditor/ckeditor5-heading';
-import { Image, ImageCaption, ImageStyle, ImageToolbar, ImageUpload } from '@ckeditor/ckeditor5-image';
-import { Indent } from '@ckeditor/ckeditor5-indent';
-import { Link } from '@ckeditor/ckeditor5-link';
-import { List } from '@ckeditor/ckeditor5-list';
 import { MediaEmbed } from '@ckeditor/ckeditor5-media-embed';
 import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
-import { Table, TableToolbar } from '@ckeditor/ckeditor5-table';
-import { Base64UploadAdapter } from '@ckeditor/ckeditor5-upload';
 
 import Video from '../src/video';
 
@@ -26,26 +17,10 @@ ClassicEditor
 		plugins: [
 			Video,
 			Essentials,
-			Autoformat,
-			BlockQuote,
 			Bold,
 			Heading,
-			Image,
-			ImageCaption,
-			ImageStyle,
-			ImageToolbar,
-			ImageUpload,
-			Indent,
-			Italic,
-			Link,
-			List,
 			MediaEmbed,
-			Paragraph,
-			Table,
-			TableToolbar,
-			CodeBlock,
-			Code,
-			Base64UploadAdapter
+			Paragraph
 		],
 		toolbar: [
 			'videoButton',
@@ -53,40 +28,9 @@ ClassicEditor
 			'heading',
 			'|',
 			'bold',
-			'italic',
-			'link',
-			'code',
-			'bulletedList',
-			'numberedList',
 			'|',
-			'outdent',
-			'indent',
-			'|',
-			'uploadImage',
-			'blockQuote',
-			'insertTable',
-			'mediaEmbed',
-			'codeBlock',
-			'|',
-			'undo',
-			'redo'
-		],
-		image: {
-			toolbar: [
-				'imageStyle:inline',
-				'imageStyle:block',
-				'imageStyle:side',
-				'|',
-				'imageTextAlternative'
-			]
-		},
-		table: {
-			contentToolbar: [
-				'tableColumn',
-				'tableRow',
-				'mergeTableCells'
-			]
-		}
+			'mediaEmbed'
+		]
 	} )
 	.then( editor => {
 		window.editor = editor;
